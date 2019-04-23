32ebb6b257b520a3358e964fb38c2da4f2cd78ff
Rahat Chowdhury <rahatchowdhury@Rahat-Chowdhurys-MacBook-Air.local>
2019-04-23 18:18:31 -0400

    added user auth protection and admin role

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 import cors from 'cors';
 import uuidv4 from 'uuid/v4';
+import jwt from 'jsonwebtoken';
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServer,
+  AuthenticationError,
+} from 'apollo-server-express';
 
 import schema from './schema';
 import resolvers from './resolvers';
@@ -12,12 +16,26 @@ const app = express();
 
 app.use(cors());
 
+const getMe = async req => {
+  const token = req.headers['x-token'];
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError(
+        'Your session expired. Sign in again.',
+      );
+    }
+  }
+};
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   formatError: error => {
-    //remove the internal sequilize error message
-    //leave only the important validation error
+    // remove the internal sequelize error message
+    // leave only the important validation error
     const message = error.message
       .replace('SequelizeValidationError: ', '')
       .replace('Validation error: ', '');
@@ -27,11 +45,15 @@ const server = new ApolloServer({
       message,
     };
   },
-  context: async () => ({
-    models,
-    me: await models.User.findByLogin('rchowdhury'),
-    secret: process.env.SECRET,
-  }),
+  context: async ({ req }) => {
+    const me = await getMe(req);
+
+    return {
+      models,
+      me,
+      secret: process.env.SECRET,
+    };
+  },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
@@ -52,11 +74,12 @@ const createUsersWithMessages = async () => {
   await models.User.create(
     {
       username: 'rchowdhury',
-      email: 'rahat@rahatcodes.com',
+      email: 'hello@rahat.com',
       password: 'rahat',
+      role: 'ADMIN',
       messages: [
         {
-          text: 'Ay yo wuz goooood',
+          text: 'I\'m Hackerman',
         },
       ],
     },
@@ -68,14 +91,14 @@ const createUsersWithMessages = async () => {
   await models.User.create(
     {
       username: 'hkhan',
-      email: 'jaan@jaan.com',
-      password: 'jaan',
+      email: 'hello@khan.com',
+      password: 'khan',
       messages: [
         {
-          text: 'I love pizza',
+          text: 'I love pizza!',
         },
         {
-          text: 'Stop eating chocolate',
+          text: 'Stop eating chocolate!',
         },
       ],
     },
