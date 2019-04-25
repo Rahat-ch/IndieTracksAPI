bf1410d36657158ff756010bcc4d40147970a065
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-04-24 21:00:04 -0400

    fixed sequilize typo

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 
 let sequelize;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequielize(process.env.DATABASE_URL, {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
   });
 } else {
